70eed0071aa6976c1165d28e9c3d470369e623dd
Muhammad Jazlan <114135875+jazlannnn@users.noreply.github.com>
2024-10-13 20:46:16 +0800

    Delete Wondrous_Bouquet_CSC264 directory

--- Wondrous_Bouquet_CSC264/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$currency = 'RM';
-$db_username = 'root';
-$db_password = '';
-$db_name = 'ecom_wondrousbouquet';
-$db_host = 'localhost';
-$mysqli = new mysqli($db_host, $db_username, $db_password ,$db_name);
-?>
